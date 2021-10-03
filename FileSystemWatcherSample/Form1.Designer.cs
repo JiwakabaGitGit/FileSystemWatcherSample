af12aeb43d9cc37155f6307e231535b27e7d6272
JiwakabaGitGit <jiwakaba.blog@gmail.com>
2021-10-03 23:45:37 +0900

    プロジェクト ファイルを追加します。

---
+++ FileSystemWatcherSample/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿
+namespace FileSystemWatcherSample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose( bool disposing )
+        {
+            if ( disposing && ( components != null ) )
+            {
+                components.Dispose();
+            }
+            base.Dispose( disposing );
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button_path = new System.Windows.Forms.Button();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.textBox_path = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button_path
+            // 
+            this.button_path.Location = new System.Drawing.Point(32, 35);
+            this.button_path.Name = "button_path";
+            this.button_path.Size = new System.Drawing.Size(80, 23);
+            this.button_path.TabIndex = 0;
+            this.button_path.Text = "Path";
+            this.button_path.UseVisualStyleBackColor = true;
+            this.button_path.Click += new System.EventHandler(this.button_path_Click);
+            // 
+            // button_start
+            // 
+            this.button_start.Location = new System.Drawing.Point(32, 79);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(75, 23);
+            this.button_start.TabIndex = 1;
+            this.button_start.Text = "Start";
+            this.button_start.UseVisualStyleBackColor = true;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.Location = new System.Drawing.Point(130, 79);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(75, 23);
+            this.button_stop.TabIndex = 2;
+            this.button_stop.Text = "Stop";
+            this.button_stop.UseVisualStyleBackColor = true;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // textBox_path
+            // 
+            this.textBox_path.Location = new System.Drawing.Point(130, 35);
+            this.textBox_path.Name = "textBox_path";
+            this.textBox_path.Size = new System.Drawing.Size(503, 22);
+            this.textBox_path.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(694, 134);
+            this.Controls.Add(this.textBox_path);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.button_path);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button_path;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.TextBox textBox_path;
+    }
+}
+
---
+++ FileSystemWatcherSample/Form1.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileSystemWatcherSample
+{
+    public partial class Form1 : Form
+    {
+        private string m_targetPath = "";
+        private System.IO.FileSystemWatcher watcher = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        ~Form1()
+        {
+            if ( watcher != null )
+            {
+                //監視を終了
+                watcher.EnableRaisingEvents = false;
+                watcher.Dispose();
+                watcher = null;
+            }
+        }
+
+        private void Initialie()
+        {
+            if ( watcher != null )
+            {
+                return;
+            }
+
+            watcher = new System.IO.FileSystemWatcher();
+            //監視するディレクトリを指定
+            watcher.Path = Path.GetDirectoryName( m_targetPath );
+            watcher.Filter = Path.GetFileName( m_targetPath );
+            //最終アクセス日時、最終更新日時、ファイル、フォルダ名の変更を監視する
+            watcher.NotifyFilter =
+                ( System.IO.NotifyFilters.LastAccess
+                | System.IO.NotifyFilters.LastWrite
+                | System.IO.NotifyFilters.FileName
+                | System.IO.NotifyFilters.DirectoryName );
+            //すべてのファイルを監視
+            watcher.Filter = "";
+            //UIのスレッドにマーシャリングする
+            //コンソールアプリケーションでの使用では必要ない
+            watcher.SynchronizingObject = this;
+
+            //イベントハンドラの追加
+            watcher.Changed += new System.IO.FileSystemEventHandler( Watcher_Changed );
+            watcher.Created += new System.IO.FileSystemEventHandler( Watcher_Changed );
+            watcher.Deleted += new System.IO.FileSystemEventHandler( Watcher_Changed );
+            watcher.Renamed += new System.IO.RenamedEventHandler( Watcher_Renamed );
+        }
+        private void button_path_Click( object sender, EventArgs e )
+        {
+            //テキストボックスをクリア
+            textBox_path.Text = "";
+
+            //ファイルダイアログを生成する
+            OpenFileDialog op = new OpenFileDialog();
+            op.Title = "ファイルを開く";
+            op.InitialDirectory = @"C:\";
+            op.Filter = "すべてのファイル(*.*)|*.*";
+            op.FilterIndex = 1;
+
+            //オープンファイルダイアログを表示する
+            DialogResult dialog = op.ShowDialog();
+
+            //「開く」ボタンが選択された時の処理
+            if ( dialog == DialogResult.OK )
+            {
+                m_targetPath = op.FileName;
+                //テキストボックスにパスを表示
+                textBox_path.Text = m_targetPath;
+            }
+        }
+
+        private void button_start_Click( object sender, EventArgs e )
+        {
+            if ( m_targetPath.Length <= 0 )
+            {
+                return;
+            }
+
+            Initialie();
+
+            //監視を開始する
+            watcher.EnableRaisingEvents = true;
+            Console.WriteLine( "監視を開始しました。" );
+        }
+
+        private void button_stop_Click( object sender, EventArgs e )
+        {
+            if ( watcher != null )
+            {
+                //監視を終了
+                watcher.EnableRaisingEvents = false;
+                Console.WriteLine( "監視を終了しました。" );
+            }
+        }
+        //イベントハンドラ
+        private void Watcher_Changed( System.Object source,
+            System.IO.FileSystemEventArgs e )
+        {
+            switch ( e.ChangeType )
+            {
+                case System.IO.WatcherChangeTypes.Changed:
+                    Console.WriteLine(
+                        "ファイル 「" + e.FullPath + "」が変更されました。" );
+                    break;
+                case System.IO.WatcherChangeTypes.Created:
+                    Console.WriteLine(
+                        "ファイル 「" + e.FullPath + "」が作成されました。" );
+                    break;
+                case System.IO.WatcherChangeTypes.Deleted:
+                    Console.WriteLine(
+                        "ファイル 「" + e.FullPath + "」が削除されました。" );
+                    break;
+            }
+        }
+
+        private void Watcher_Renamed( System.Object source,
+            System.IO.RenamedEventArgs e )
+        {
+            Console.WriteLine(
+                "ファイル 「" + e.FullPath + "」の名前が変更されました。" );
+        }
+
+    }
+}
